805f2df645dec0a947219affc4042f531fb31d96
Tu Vu <vuanhtu52@gmail.com>
2023-07-16 17:50:41 +0700

    Complete function operate

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -13,4 +17,18 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*": 
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
+
 
